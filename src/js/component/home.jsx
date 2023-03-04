e6391d3faf5f3438478fd6df6cba1f8e68dad7aa
WyattChase <esnaruto@yahoo.com>
2023-03-04 00:56:33 +0000

    final before bonus

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,21 +5,29 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const { selectedColor, setSelectedColor } = useState("red");
+  const [ Color, setColor ] = useState("");
 
-	return (
-		<div className="traffic-light">
-			<div className="container">
-				<div className={"light red" + { selectedColor } === "red" ? "glow" : ""} />
-					</div>
-				<div className={"light yellow" + { selectedColor } === "red" ? "glow" : ""} />
-				</div>
-				<div className={"light green" + { selectedColor } === "red" ? "glow" : ""} />
-				</div>
-			</div>
-			
-		</div>
-	);
+  return (
+	<><div className="traffic-top"> </div><div className="traffic-light">
+		  <div className="container">
+			  <div
+				  onClick={() => setColor("red")}
+				  className={"light red" + (Color === "red" ? " glow" : "")}
+			  >
+			  </div>
+			  <div
+				  onClick={() => setColor("yellow")}
+				  className={"light yellow" + (Color === "yellow" ? " glow" : "")}
+			  >
+			  </div>
+			  <div
+				  onClick={() => setColor("green")}
+				  className={"light green" + (Color === "green" ? " glow" : "")}
+			  >
+			  </div>
+		  </div>
+	  </div></>
+  );
 };
 
 export default Home;
